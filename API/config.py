21af27f283b38ae41d651138197bf6f32a0c2bab
Hanan4ik <nektovich3456@gmail.com>
2026-05-13 11:37:57 +0300

    Start making logic of config validation

--- API/config.py
+++ API/config.py
@@ -1,11 +1,31 @@
 from os.path import exists
 from os import mkdir
+from helpers import dictify_all, client_param, server_link
+from requests import get as GET
+
+def check_config():
+    if not exists("config"): mkdir("config")
+ 
+
+def check_client_config() -> bool:
+    check_config()
+    if not exists("config/client.conf"):
+        return False
+    if client_param("limitIp") is None:
+        return False
+    if client_param("totalGB") is None:
+        return False
+    if client_param("reset") is None:
+        return False
+
+    return True
+
+
 
 def generate_client_config(force: bool = False) -> None:
-    if not exists("config"): mkdir("config")
-    if exists("config/client.conf") and not force:
-        print("Client config already present")
-        return
+
+    if check_client_config(): return
+
     with open("config/client.conf", "w") as f:
         print("Let's create client.conf file")
         if input("Do you wish to continue [Y/n]").lower() not in "y ":
@@ -16,14 +36,24 @@ def generate_client_config(force: bool = False) -> None:
         reset = input("Enter how frequently traffic should be reset (0 for no reset): ")
         f.write(f"limitIp={limitIp}\ntotalGB={totalGB}\nreset={reset}")
 
+def check_server_config() -> bool:
+    check_config()
+    if not exists("config/server_list.conf"):
+        return False
+    for server in dictify_all():    
+        if GET(server_link(server)).status_code != 200:
+            return False
+
+    return True
+    
+
 # TODO inline server assignment
 # TODO adding new servers
 def generate_servers_config(force: bool = False) -> None:
-    if not exists("config"): mkdir("config")
-    if exists("config/server_list.conf") and not force:
-        print("Servers already assigned")
+    
+    if check_server_config() and not force:
         return
-
+    
     with open("config/server_list.conf", "w") as f:
 
 
@@ -36,7 +66,7 @@ def generate_servers_config(force: bool = False) -> None:
 
         print("Can't find server_list.conf. Let's create it from scratch")
         cnt = 0
-        while input("Do you wish to continue? [Y/n]").lower() != "n":
+        while input("Do you wish to continue? [Y/n] ").lower() != "n":
             proto = input("Enter protocol of accessing panel (http/https): ")
             domain = input("Enter ip/domain of server: ")
             port = input("Enter panel access port: ")
@@ -53,14 +83,28 @@ def generate_servers_config(force: bool = False) -> None:
         else:
             print(f"Added {cnt} server{"s" if cnt != 1 else ""}")
 
-
+# Assumes that server list already assigned
 def generate_subscription_config(force: bool = False) -> None:
     if not exists("config"): mkdir("config")
     if exists("config/subscription.conf") and not force:
         print("Subscription service already configured")
         return
+    
+    print("Can't find subscription.conf. Let's create it from scratch")
+    if input("Do you wish to continue? [Y/n] ").lower() == "n":
+        return
+    with open("config/subscription.conf", "w") as f:
+        title = input("Enter subscription title: ")
+        desc = input("Enter subscription description: ")
+        #sub -> domain:port/sub_path
+        for server in dictify_all():
+            print("Set subscription url for", server["domain"])
+            port = input("Enter subscription port for", server['domain']+": ")
+            sub_path = input("Enter subscription secret path for," server['domain']+": ")
+            f.write(f"{server['proto']}://{server['domain']}:{port}/{sub_path}/") # append sub_id of client
 
-    with open("config/server_list.conf", "w") as f:
+
+
 
 
 
